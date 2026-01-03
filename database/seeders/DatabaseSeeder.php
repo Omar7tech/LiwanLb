861b3c0e7d7a0da348eeb810e00de377acd6caa5
Omar Abi Farraj <omar.7tech@gmail.com>
2026-01-03 23:47:45 +0200

    latest one lets see

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,17 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Work;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $superAdminRole = \Spatie\Permission\Models\Role::firstOrCreate(['name' => 'super_admin']);
         $clientRole = \Spatie\Permission\Models\Role::firstOrCreate(['name' => 'client']);
         $foremanRole = \Spatie\Permission\Models\Role::firstOrCreate(['name' => 'foreman']);
         $bloggerRole = \Spatie\Permission\Models\Role::firstOrCreate(['name' => 'blogger']);
-        
         $admin = User::firstOrCreate(
             ['email' => 'liwan@admin.com'],
             [
@@ -32,8 +27,6 @@ class DatabaseSeeder extends Seeder
         if (!$admin->hasRole('super_admin')) {
             $admin->assignRole('super_admin');
         }
-        
-
         $this->call([
             WorkSeeder::class,
         ]);
